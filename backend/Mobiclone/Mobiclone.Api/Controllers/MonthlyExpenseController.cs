a0eed58af47f5ef9067217634ce1a20c887aabe9
flaviogf <flavio.fernandes6@gmail.com>
2019-12-19 22:01:44 -0200

    feat: finishing monthly expense

--- backend/Mobiclone/Mobiclone.Api/Controllers/MonthlyExpenseController.cs
+++ backend/Mobiclone/Mobiclone.Api/Controllers/MonthlyExpenseController.cs
@@ -1,14 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mobiclone.Api.Database;
+using Mobiclone.Api.Lib;
+using Mobiclone.Api.ViewModels;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Mobiclone.Api.Controllers
 {
+    [ApiController]
+    [Route("monthly-expense")]
+    [Authorize]
     public class MonthlyExpenseController : Controller
     {
+        private readonly MobicloneContext _context;
+
+        private readonly IAuth _auth;
+
+        public MonthlyExpenseController(MobicloneContext context, IAuth auth)
+        {
+            _context = context;
+            _auth = auth;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseViewModel<int>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Show(DateTime start, DateTime end)
         {
-            return Ok("");
+            var user = await _auth.User();
+
+            var expenses = await (from expense in _context.Expenses
+                                  where expense.Account.User == user && expense.Date >= start && expense.Date <= end
+                                  select expense.Value).SumAsync();
+
+            var response = new ResponseViewModel<int>(expenses);
+
+            return Ok(response);
         }
     }
 }
--- backend/Mobiclone/Mobiclone.Test/Integration/MonthlyExpenseControllerTests.cs
+++ backend/Mobiclone/Mobiclone.Test/Integration/MonthlyExpenseControllerTests.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Mobiclone.Api.Controllers;
 using Mobiclone.Api.Database;
+using Mobiclone.Api.Lib;
+using Mobiclone.Api.ViewModels;
 using System;
 using Xunit;
 
@@ -16,6 +20,8 @@ namespace Mobiclone.Test.Integration
 
         private readonly SqliteConnection _connection;
 
+        private readonly HttpContextAccessor _accessor;
+
         public MonthlyExpenseControllerTests()
         {
             _connection = new SqliteConnection("Data Source=:memory:");
@@ -26,7 +32,18 @@ namespace Mobiclone.Test.Integration
 
             _context = new MobicloneContext(options);
 
-            _controller = new MonthlyExpenseController();
+            var hash = new Bcrypt();
+
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.Test.json").Build();
+
+            _accessor = new HttpContextAccessor
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            var auth = new Jwt(_context, hash, configuration, _accessor);
+
+            _controller = new MonthlyExpenseController(_context, auth);
 
             _context.Database.EnsureCreated();
         }
@@ -39,6 +56,20 @@ namespace Mobiclone.Test.Integration
             await _context.Users.AddAsync(user);
 
             await _context.SaveChangesAsync();
+
+            var account = await Factory.Account(userId: user.Id);
+
+            await _context.Accounts.AddAsync(account);
+
+            await _context.SaveChangesAsync();
+
+            var expense = await Factory.Expense(accountId: account.Id, date: new DateTime(year: 2019, month: 12, day: 15), value: 180000);
+
+            await _context.Expenses.AddAsync(expense);
+
+            await _context.SaveChangesAsync();
+
+            _accessor.HttpContext.User = await Factory.ClaimsPrincipal(userId: user.Id);
 
             var start = new DateTime(year: 2019, month: 12, day: 1);
 
@@ -58,13 +89,29 @@ namespace Mobiclone.Test.Integration
 
             await _context.SaveChangesAsync();
 
+            var account = await Factory.Account(userId: user.Id);
+
+            await _context.Accounts.AddAsync(account);
+
+            await _context.SaveChangesAsync();
+
+            var expense = await Factory.Expense(accountId: account.Id, date: new DateTime(year: 2019, month: 12, day: 15), value: 180000);
+
+            await _context.Expenses.AddAsync(expense);
+
+            await _context.SaveChangesAsync();
+
+            _accessor.HttpContext.User = await Factory.ClaimsPrincipal(userId: user.Id);
+
             var start = new DateTime(year: 2019, month: 12, day: 1);
 
             var end = new DateTime(year: 2019, month: 12, day: 31);
 
             var result = await _controller.Show(start, end);
 
-            Assert.IsAssignableFrom<OkObjectResult>(result);
+            var response = Assert.IsAssignableFrom<OkObjectResult>(result);
+
+            Assert.Equal(180000, ((ResponseViewModel<int>)response.Value).Data);
         }
 
         public void Dispose()
